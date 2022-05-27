2770296eebe0cd997155c9653195b70453bbba51
Drastick17 <danny12solano@gmail.com>
2022-05-27 11:19:45 -0500

    Updates

--- index.js
+++ index.js
@@ -4,44 +4,52 @@ const path = require('path')
 const cors = require('cors')
 const fs = require('fs')
 
-const PORT = process.env.PORT || 3005
+const PORT = process.env.PORT || 3000
 const app = express()
 app.use(cors())
 app.use(express.static("public"));
 
 const PATH = (file) => path.join(`${__dirname}/${file}`)
-rmEmoji = (str) => str.replace(/\p{EPres}|\p{ExtPict}/gu,'');
+rmEmoji = (str) => str.replace(/\p{EPres}|\p{ExtPict}/gu,'')
+rmEspecialsC = (str) => str.replace(/[^a-zA-Z ]/g, "")
 
 app.get('/', (req, res) =>{
   res.sendFile(PATH('index.html'))
 })
+
 //Download 
-app.get('/video', (req, res) => {
+app.get('/download', (req, res) => {
   const {url, quality} = req.query
   let title = "";
   ytdl.getInfo(url).then(res => {
-    title = rmEmoji(res.videoDetails.title)
+    title = rmEspecialsC(rmEmoji(res.videoDetails.title))
   }).then(() =>{
-    res.header("Content-Disposition", `attachment;\  filename="${title}.mp4`)    
+    res.header("Content-Disposition", `attachment;  filename="${title}.mp4"`)    
     ytdl(url, {format: 'mp4', quality:quality}).pipe(res)
-  }) 
+  })
 });
+
 //Get video INFO
-app.post('/video', (req, res) => {
+
+app.get('/video', (req, res) => {
   const {url} = req.query
   let VideoData = {}
   try{
     if(ytdl.validateURL(url)){
       ytdl.getInfo(url).then(info => {
         let formats = {}
+        
         info.formats.map(format => {formats[format.quality] = {
           quality : format.qualityLabel,
           format  : format.container,
-          itag : format.itag
-        }} )
-  
+          itag : format.itag,
+          contentLenght: format.contentLength ? (format.contentLength/Math.pow(1024,2)).toFixed(2): 0 ,
+          status: 200
+        }})
+
         formats = Object.keys(formats).map(format =>formats[format])
         formats = formats.filter(format => format.format === 'mp4')
+
         VideoData = {
           iframe   : info.videoDetails.embed.iframeUrl,
           url      : info.videoDetails.video_url,
@@ -49,10 +57,14 @@ app.post('/video', (req, res) => {
           duration : (info.videoDetails.lengthSeconds/60).toFixed(2),
           qualities: formats
         }
-        res.status(200).send(JSON.stringify(VideoData))})
+        res.send(VideoData)
+
+      })
+    }else{
+      res.send({error:'Error video no econtrado', status:404})
     }
   }catch(e){
-    res.send({error:'Error video no econtrado'})
+    console.log(e)
   }
   
 })
--- public/ytdl.js
+++ public/ytdl.js
@@ -2,22 +2,31 @@ const $ = (selector) => document.querySelector(selector)
 
 const baseURL = window.location.href
 
+const inputURL = $('.URL')
+
+inputURL.addEventListener('keyup', (e) => {
+  if(e.keyCode == 13 ){
+    $('.send').click()
+  }
+})
+
 $('.send').addEventListener('click', function(){
-  const url = $('.URL').value
+  const url = inputURL.value
   const content = $('.details')
   content.innerHTML = "<div class='loader'></div>"
+  
   fetch(`${baseURL}video?url=${url}`,{
     headers:{'Content-Type':'application/json'},
-    method:'POST'
+    method:'get'
   })
   .then(res => res.json())
   .then(data => {
-    if(JSON.stringify(data) === "{error:'Error video no econtrado'}"){
-      content.innerHTML = `<h3>${data.error}</h3>`
+    if(data.status === 404){
+      content.innerHTML = `<h3 style="color:#ffffff">${data.error}</h3>`
     }else{
       const {url, title, iframe, duration, qualities} = data
       content.innerHTML =`
-    <form action="/video" method="GET" class="form">
+    <div class="form">
       <div class="video">
         <input type="text" value="${url}" name="url" class="hidden"/>
         <iframe src="${iframe}" ></iframe>
@@ -27,13 +36,27 @@ $('.send').addEventListener('click', function(){
         <p class="duration"><i class="fas fa-clock"></i> ${duration} minutos</p>
         <div class="options">
           <select class="qualities" name="quality">
-            ${qualities.map(quality => `<option value="${quality.itag}">${quality.quality}</option>`)}
+            ${qualities.map(quality => `<option value="${quality.itag}">${quality.quality} / ${quality.contentLenght?`${quality.contentLenght} MB`:'Not found'}</option>`)}
           </select>
-          <button class="download">Download <i class="fas fa-download"></i></button>
+          <button class="download"><a href="${baseURL}download?url=${url}&itag=${()=>{
+              let selectedOption
+              $('.qualities').addEventListener('change', function(){
+                selectedOption = this.options[select.selectedIndex];
+              })
+              console.log(selectedOption)
+              return selectedOption
+            }
+          }}">Download <i class="fas fa-download"></i></a>
+          </button>
         </div>
       </div>
-    </form>
+    </div>
+
     `
     }
   })
 })
+
+
+
+
