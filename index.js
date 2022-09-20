5b773bfabb546dac56aafa6a9a060af61f167c12
Drastick17 <danny12solano@gmail.com>
2022-09-20 09:28:46 -0500

    videodownloader

--- index.js
+++ index.js
@@ -1,73 +1,153 @@
-const express = require('express')
-const ytdl = require('ytdl-core')
-const path = require('path')
-const cors = require('cors')
-const fs = require('fs')
+require("dotenv").config();
 
-const PORT = process.env.PORT || 3000
-const app = express()
-app.use(cors())
+const express = require("express");
+const ytdl = require("ytdl-core");
+const path = require("path");
+const cors = require("cors");
+const {
+  createWriteStream,
+  statSync,
+  createReadStream,
+  unlinkSync,
+} = require("fs");
+
+const PORT = process.env.PORT || 3002;
+const app = express();
+app.use(cors());
+
 app.use(express.static("public"));
+app.use(express.json());
 
-const PATH = (file) => path.join(`${__dirname}/${file}`)
-rmEmoji = (str) => str.replace(/\p{EPres}|\p{ExtPict}/gu,'')
-rmEspecialsC = (str) => str.replace(/[^a-zA-Z ]/g, "")
+const PATH = (file) => path.join(`${__dirname}/${file}`);
+rmEmoji = (str) => str.replace(/\p{EPres}|\p{ExtPict}/gu, "");
+rmEspecialsC = (str) => str.replace(/[^a-zA-Z ]/g, "");
 
-app.get('/', (req, res) =>{
-  res.sendFile(PATH('index.html'))
-})
-
-//Download 
-app.get('/download', (req, res) => {
-  const {url, quality} = req.query
-  let title = "";
-  ytdl.getInfo(url).then(res => {
-    title = rmEspecialsC(rmEmoji(res.videoDetails.title))
-  }).then(() =>{
-    res.header("Content-Disposition", `attachment;  filename="${title}.mp4"`)    
-    ytdl(url, {format: 'mp4', quality:quality}).pipe(res)
-  })
+app.get("/", (req, res) => {
+  res.sendFile(PATH("index.html"));
 });
 
-//Get video INFO
+//Download
+app.get("/download", async (req, res) => {
+  const { url, itag } = req.query;
 
-app.get('/video', (req, res) => {
-  const {url} = req.query
-  let VideoData = {}
-  try{
-    if(ytdl.validateURL(url)){
-      ytdl.getInfo(url).then(info => {
-        let formats = {}
-        
-        info.formats.map(format => {formats[format.quality] = {
-          quality : format.qualityLabel,
-          format  : format.container,
-          itag : format.itag,
-          contentLenght: format.contentLength ? (format.contentLength/Math.pow(1024,2)).toFixed(2): 0 ,
-          status: 200
-        }})
+  const info = await ytdl.getInfo(url);
 
-        formats = Object.keys(formats).map(format =>formats[format])
-        formats = formats.filter(format => format.format === 'mp4')
+  let title = rmEspecialsC(rmEmoji(info.videoDetails.title));
 
-        VideoData = {
-          iframe   : info.videoDetails.embed.iframeUrl,
-          url      : info.videoDetails.video_url,
-          title    : info.videoDetails.title,
-          duration : (info.videoDetails.lengthSeconds/60).toFixed(2),
-          qualities: formats
-        }
-        res.send(VideoData)
+  videoInfo = info.formats.filter((format) => format.itag === Number(itag));
 
-      })
-    }else{
-      res.send({error:'Error video no econtrado', status:404})
+  const download = ytdl(url, {
+    filter: (format) => format.itag === Number(itag),
+  });
+
+  download
+    .pipe(createWriteStream(path.resolve(__dirname, `./videos/${title}.mp4`)))
+    .on("finish", () => {
+      const { size } = statSync(
+        path.resolve(__dirname, `./videos/${title}.mp4`)
+      );
+      res.writeHead(200, {
+        "Content-Length": size,
+        "Content-Type": videoInfo[0].mimeType,
+        //"Content-Disposition": `attachment;  filename="${title}.mp4"`,
+      });
+      createReadStream(path.resolve(__dirname, `./videos/${title}.mp4`))
+        .pipe(res)
+        .on("finish", () => {
+          unlinkSync(path.resolve(__dirname, `./videos/${title}.mp4`));
+          res.end();
+        });
+    });
+
+  // ytdl.getInfo(url,{format: 'mp4'}).then(info =>{
+  //   let title = rmEspecialsC(rmEmoji(info.videoDetails.title))
+  //   ytdl(url,{ quality })
+  //   .on('response',function(res){
+  //     let ProgressBar = require('progress');
+  //     bar = new ProgressBar('downloading [:bar] :percent :etas', {
+  //       complete : String.fromCharCode(0x2588),
+  //       total    : parseInt(res.headers['content-length'], 10)
+  //     })
+  //   })
+
+  //   .on( 'data', function(data){
+  //     bar.tick(data.length);
+  //   })
+  //   .on( 'finish', function(){ console.log('Download finished...'); } )
+  //   .pipe(createWriteStream(`${title}.mp4`));
+
+  //   // ytdl(url,{ quality }).pipe(createWriteStream(`./videos/${title}.mp4`))
+  //   // res.header("Content-Disposition", `attachment;  filename="${title}.mp4"`)
+  //   // ytdl(url,{ quality }).pipe()
+
+  //   // videoReadable.on('end', () =>{
+  //   //   videoReadable.pipe(fs.createReadStream(title))
+  //   //   res.header("Content-Disposition", `attachment;  filename="${title}.mp4"`)
+  //   //   res.header("Content-Length",title.length)
+  //   // })
+  // })
+  // ytdl.getInfo(url,{format: 'mp4', quality:quality}, (err,info)=>{
+  //   let video = ytdl.downloadFromInfo(info, {
+  //     quality: quality
+  //    })
+  //    const videoStream = fs.createReadStream(video)
+  //    videoStream.on(;da)
+  //    videoStream.on('end', () =>{
+  //     videoStream.pipe
+  //    })
+
+  // })
+  // ytdl.getInfo(url).then(res => {
+  //   title = rmEspecialsC(rmEmoji(res.videoDetails.title))
+  // }).then(() =>{
+  //   console.log(res)
+  //   res.header("Content-Disposition", `attachment;  filename="${title}.mp4"`)
+  //   ytdl.
+  //   // const videoStream = ytdln(url, {format: 'mp4', quality:quality}).pipe(fs.createReadStream(title))
+
+  //   console.log(videoStream)
+  // })
+});
+
+app.post("/video", async (req, res) => {
+  const { url } = req.body;
+  let formats = {};
+
+  try {
+    if (!ytdl.validateURL(url)) {
+      res.json({ status: 404, message: "Error video no encontrado" });
+      res.end();
     }
-  }catch(e){
-    console.log(e)
+
+    const { formats: videoFormats, videoDetails } = await ytdl.getInfo(url);
+
+    videoFormats.map(
+      ({ quality, qualityLabel, container, itag, contentLength }) => {
+        formats[quality] = {
+          quality: qualityLabel,
+          format: container,
+          itag: itag,
+          contentLenght: contentLength || "no found",
+        };
+      }
+    );
+
+    formats = Object.keys(formats)
+      .map((format) => formats[format])
+      .filter((format) => format.format === "mp4");
+
+    res.json({
+      iframe: videoDetails.embed.iframeUrl,
+      url: videoDetails.video_url,
+      title: videoDetails.title,
+      duration: (videoDetails.lengthSeconds / 60).toFixed(2),
+      qualities: formats,
+      status: 200,
+    });
+  } catch (e) {
+    console.error(e);
+    res.json({ status: 500, message: "Error en el servidor" });
   }
-  
-})
+});
 
-
-app.listen(PORT,() => console.log(`http://localhost:${PORT}`))
+app.listen(PORT, () => console.log(`http://localhost:${PORT}`));
--- public/ytdl.js
+++ public/ytdl.js
@@ -1,62 +1,133 @@
-const $ = (selector) => document.querySelector(selector)
+const $ = (selector) => document.querySelector(selector);
 
-const baseURL = window.location.href
+const baseURL = window.location.origin;
+const inputURL = $(".URL");
+const content = $(".details");
 
-const inputURL = $('.URL')
 
-inputURL.addEventListener('keyup', (e) => {
-  if(e.keyCode == 13 ){
-    $('.send').click()
-  }
-})
-
-$('.send').addEventListener('click', function(){
-  const url = inputURL.value
-  const content = $('.details')
-  content.innerHTML = "<div class='loader'></div>"
-  
-  fetch(`${baseURL}video?url=${url}`,{
-    headers:{'Content-Type':'application/json'},
-    method:'get'
-  })
-  .then(res => res.json())
-  .then(data => {
-    if(data.status === 404){
-      content.innerHTML = `<h3 style="color:#ffffff">${data.error}</h3>`
-    }else{
-      const {url, title, iframe, duration, qualities} = data
-      content.innerHTML =`
-    <div class="form">
+const templateForm = ({ iframe, url, title, duration, qualities }) => `
+<form class="form" id="download">
       <div class="video">
         <input type="text" value="${url}" name="url" class="hidden"/>
         <iframe src="${iframe}" ></iframe>
       </div>
       <div>
         <p class="title">${title}</p>
-        <p class="duration"><i class="fas fa-clock"></i> ${duration} minutos</p>
+        <p class="duration"><i class="fas fa-clock"></i> ${duration}minutos</p>
         <div class="options">
-          <select class="qualities" name="quality">
-            ${qualities.map(quality => `<option value="${quality.itag}">${quality.quality} / ${quality.contentLenght?`${quality.contentLenght} MB`:'Not found'}</option>`)}
+          <select class="qualities" name="qualities">
+            ${qualities.map(
+              ({ quality, format, itag, contentLenght }) =>
+                `<option value="${itag}">${quality}/ ${(
+                  contentLenght /
+                  1024 ** 2
+                )?.toFixed(2)}  ${format}</option>`
+            )}
           </select>
-          <button class="download"><a href="${baseURL}download?url=${url}&itag=${()=>{
-              let selectedOption
-              $('.qualities').addEventListener('change', function(){
-                selectedOption = this.options[select.selectedIndex];
-              })
-              console.log(selectedOption)
-              return selectedOption
-            }
-          }}">Download <i class="fas fa-download"></i></a>
+          <button type="submit" class="download">
+          Descargar 
           </button>
         </div>
       </div>
-    </div>
+      </form>
+    
+`;
 
-    `
-    }
-  })
-})
+function download(blob) {
+  const url = window.URL.createObjectURL(blob);
+  const a = document.createElement('a');
+  a.style.display = 'none';
+  a.href = url;
+  a.download = $(".title").innerHTML;
+  document.body.appendChild(a);
+  a.click();
+  document.body.removeChild(a);
+  window.URL.revokeObjectURL(url);
+}
 
+async function downloadVideo(e) {
+  e.preventDefault();
+  const { url, qualities } = e.target;
+  //window.location.replace(`${baseURL}/download?url=${url.value}&&itag=${qualities.value}`)
+  content.innerHTML+= "<p>Su video se esta descargando, espere porfavor</p>"
+  const res = await fetch(`${baseURL}/download?url=${url.value}&&itag=${qualities.value}`, {
+  method: "GET",
+  });
+  const video = await res.blob()
+  console.log(video)
+  download(video)
+  content.innerHTML= ""
+}
 
+inputURL.addEventListener("keyup", (e) => {
+  if (e.keyCode == 13) {
+    $(".send").click();
+  }
+});
 
+$(".send").addEventListener("click", async function () {
+  const url = inputURL.value;
+  content.innerHTML = "<div class='loader'></div>";
+  try {
+    const res = await fetch(`${baseURL}/video`, {
+      headers: { "Content-Type": "application/json" },
+      method: "POST",
+      body: JSON.stringify({ url }),
+    });
 
+    const data = await res.json();
+
+    if (data.status === 500)
+      content.innerHTML = `<h3 style="color:#ffffff">Error del servidor</h3>`;
+    if (data.status === 404)
+      content.innerHTML = `<h3 style="color:#ffffff">Error: Video no encontrado o borrado</h3>`;
+    if (data.status === 200) content.innerHTML = templateForm(data);
+    $("#download").addEventListener("submit", downloadVideo);
+  } catch (error) {
+    content.innerHTML = `<h3 style="color:#ffffff">Video no encontrado</h3>`;
+  }
+
+  // fetch(`${baseURL}video?url=${url}`,{
+  //   headers:{'Content-Type':'application/json'},
+  //   method:'get'
+  // })
+  // .then(res => res.json())
+  // .then(data => {
+  //   if(data.status === 404){
+  //     content.innerHTML = `<h3 style="color:#ffffff">${data.error}</h3>`
+  //   }else{
+  //     const {url, title, iframe, duration, qualities} = data
+  //     content.innerHTML =`
+  //   <div class="form">
+  //     <div class="video">
+  //       <input type="text" value="${url}" name="url" class="hidden"/>
+  //       <iframe src="${iframe}" ></iframe>
+  //     </div>
+  //     <div>
+  //       <p class="title">${title}</p>
+  //       <p class="duration"><i class="fas fa-clock"></i> ${duration} minutos</p>
+  //       <div class="options">
+  //         <select class="qualities" name="quality">
+  //           ${qualities.map(quality => `<option value="${quality.itag}">${quality.quality} / ${quality.contentLenght?`${quality.contentLenght} MB`:'Not found'}</option>`)}
+  //         </select>
+  //         <button class="download"><a href="${baseURL}download?url=${url}&itag=${()=>{
+  //             let selectedOption
+  //             $('.qualities').addEventListener('change', function(){
+  //               selectedOption = this.options[select.selectedIndex];
+  //             })
+  //             console.log(selectedOption)
+  //             return selectedOption
+  //           }
+  //         }}">Download <i class="fas fa-download"></i></a>
+  //         </button>
+  //       </div>
+  //     </div>
+  //   </div>
+
+  //   `
+  //   }
+  // })
+  // .catch(() =>{
+  //   content.innerHTML = `<h3 style="color:#ffffff">"Video no encontrado o borrado"</h3>`
+  // })
+});
